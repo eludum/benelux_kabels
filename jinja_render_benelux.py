81be370e6adc9ad75351382bed1e931e1c690cdf
eludum <mehmet003@hotmail.be>
2021-10-03 13:11:28 +0200

    add jinja2 rendering to all pages

--- jinja_render_benelux.py
+++ jinja_render_benelux.py
@@ -1,4 +1,15 @@
 from jinja2 import Environment, Template, FileSystemLoader
 env = Environment(loader = FileSystemLoader(["."]))
+
 t = env.get_template("index.html")
 print(t.render(base_template="base.html"))
+
+t = env.get_template("projecten.html")
+print(t.render(base_template="base.html"))
+
+t = env.get_template("veiligheid.html")
+print(t.render(base_template="base.html"))
+
+t = env.get_template("contact.html")
+print(t.render(base_template="base.html"))
+
