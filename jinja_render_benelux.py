f47184d4fccb35328cceb9b8755b5db0948017f5
eludum <mehmet003@hotmail.be>
2021-10-20 16:39:37 +0200

    cont

--- jinja_render_benelux.py
+++ jinja_render_benelux.py
@@ -1,12 +1,26 @@
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, Template, FileSystemLoader
 env = Environment(loader = FileSystemLoader(["."]))
 
-base = "base.html"
-sites = ["index.html", "projecten.html", "veiligheid.html", "contact.html"]
+t = env.get_template("index.html")
+index = t.render(base_template="base.html")
 
-for site in sites:
-    template = env.get_template(site)
-    rendered_template = template.render(base_template=base)
+t = env.get_template("projecten.html")
+projects = t.render(base_template="base.html")
 
-    with open("rendered/" + site, "w") as fh:
-        fh.write(rendered_template)
+t = env.get_template("veiligheid.html")
+safety = t.render(base_template="base.html")
+
+t = env.get_template("contact.html")
+contact = t.render(base_template="base.html")
+
+with open("rendered/index.html", "w") as fh:
+    fh.write(index)
+
+with open("rendered/projecten.html", "w") as fh:
+    fh.write(projects)
+
+with open("rendered/veiligheid.html", "w") as fh:
+    fh.write(safety)
+
+with open("rendered/contact.html", "w") as fh:
+    fh.write(contact)
