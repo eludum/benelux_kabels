a930c01ebfd39c31728df6da00376de1645a8098
GKSM <mehmet.kose@mazars.be>
2021-10-20 16:44:12 +0200

    lol

--- jinja_render_benelux.py
+++ jinja_render_benelux.py
@@ -1,26 +1,33 @@
-from jinja2 import Environment, Template, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader
 env = Environment(loader = FileSystemLoader(["."]))
 
-t = env.get_template("index.html")
-index = t.render(base_template="base.html")
+base = "base.html"
+sites = ["index.html", "projecten.html", "veiligheid.html", "contact.html"]
 
-t = env.get_template("projecten.html")
-projects = t.render(base_template="base.html")
+projects_gallery = [{'1': 'Trekken van zware vermogen-kabels op kabelbaan in Tessenderlo.',
+                '2': 'Trekken van zware vermogen-kabels op kabelbaan in Dendermonde.',
+                '3': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.',
+                '4': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.'},
+               {'1': 'Trekken van zware vermogen-kabels op kabelbaan in Tessenderlo.',
+                '2': 'Trekken van zware vermogen-kabels op kabelbaan in Dendermonde.',
+                '3': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.',
+                '4': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.'},
+               {'1': 'Trekken van zware vermogen-kabels op kabelbaan in Tessenderlo.',
+                '2': 'Trekken van zware vermogen-kabels op kabelbaan in Dendermonde.',
+                '3': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.',
+                '4': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.'},
+               {'1': 'Trekken van zware vermogen-kabels op kabelbaan in Tessenderlo.',
+                '2': 'Trekken van zware vermogen-kabels op kabelbaan in Dendermonde.',
+                '3': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.',
+                '4': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.'},
+               {'1': 'Trekken van zware vermogen-kabels op kabelbaan in Tessenderlo.',
+                '2': 'Trekken van zware vermogen-kabels op kabelbaan in Dendermonde.',
+                '3': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.',
+                '4': 'Trekken en vastleggen van zware vermogen-kabels, aarding in sleuf en kabelbaan in Tessenderlo.'},]
 
-t = env.get_template("veiligheid.html")
-safety = t.render(base_template="base.html")
+for site in sites:
+    template = env.get_template(site)
+    rendered_template = template.render(base_template=base, projects_gallery=projects_gallery)
 
-t = env.get_template("contact.html")
-contact = t.render(base_template="base.html")
-
-with open("rendered/index.html", "w") as fh:
-    fh.write(index)
-
-with open("rendered/projecten.html", "w") as fh:
-    fh.write(projects)
-
-with open("rendered/veiligheid.html", "w") as fh:
-    fh.write(safety)
-
-with open("rendered/contact.html", "w") as fh:
-    fh.write(contact)
+    with open("rendered/" + site, "w") as fh:
+        fh.write(rendered_template)
